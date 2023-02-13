5d00d0aefd15b1f285ec189fc5f02fe7e081b54b
scmylsc <124163547+scmylsc@users.noreply.github.com>
2023-02-13 15:29:15 +0530

    Update password.py

--- user_data/password.py
+++ user_data/password.py
@@ -1,4 +1,3 @@
-# 
 import re
 from username import username
 def password():
@@ -12,3 +11,20 @@ class send_msg:
         username2 = int(username)
     def msg_wa(password1,username1):
         print(int(username1) + int(password))
+ class collect_num():
+    def __init__(no,name):
+        this.no = number
+        this.name = user_name
+     def send(number,user_name):
+        
+        
+if __name__ == "__main__":
+    noo = int(input("Enter Your Number"))
+    if(length(noo) < 10):
+        return 1
+    else if(length(noo) > 10):
+        return 2
+    else :
+        return 0
+    name = str(("Enter your Name: "))
+    collect(noo,name)
